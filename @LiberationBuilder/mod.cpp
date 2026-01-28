b905f9f5d5dc76efb01814cc61a68b5f1f05b900
Juan Antonio López <juanantonio.lopez@onestic.com>
2026-01-28 19:32:19 +0100

    Add workshop preview image and update mod.cpp

--- @LiberationBuilder/mod.cpp
+++ @LiberationBuilder/mod.cpp
@@ -1,15 +1,15 @@
 name = "Liberation Builder Helper";
 author = "LBH Team";
-url = "https://github.com/liberation-builder-helper";
+url = "https://github.com/jals-es/mod-liberation-builder-helper";
 actionName = "GitHub";
 
 dir = "@LiberationBuilder";
-picture = "";
+picture = "workshop_preview.png";
 hidePicture = "false";
 hideName = "false";
-logo = "";
-logoOver = "";
-logoSmall = "";
+logo = "workshop_preview.png";
+logoOver = "workshop_preview.png";
+logoSmall = "workshop_preview.png";
 tooltip = "Liberation Builder Helper";
 tooltipOwned = "Liberation Builder Helper - Owned";
 overview = "A mod to help build KP Liberation missions by collecting classnames from 3DEN Editor and ACE Arsenal.";
